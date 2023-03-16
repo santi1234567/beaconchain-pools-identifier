e52acb83f46b77de87412d525726c54c46946da9
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2023-03-16 13:52:31 -0300

    Added poolidentifier integration

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/santi1234567/eth-pools-identifier/config"
+	"github.com/santi1234567/eth-pools-identifier/poolIdentifier"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -20,7 +22,13 @@ func main() {
 	}
 	log.SetLevel(logLevel)
 
+	poolIdentifier, err := poolIdentifier.NewPoolIdentifier(context.Background(), config)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	go poolIdentifier.Run()
 	// Wait for signal.
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
---
+++ poolIdentifier/poolIdentifier.go
@@ -0,0 +1,107 @@
+package poolIdentifier
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/santi1234567/eth-pools-identifier/config"
+	"github.com/santi1234567/eth-pools-identifier/postgresql"
+
+	log "github.com/sirupsen/logrus"
+)
+
+
+type PoolIdentifier struct {
+	postgresql     *postgresql.Postgresql
+	
+	config     *config.Config // TODO: Remove repeated parameters
+}
+
+func NewPoolIdentifier(
+	ctx context.Context,
+	config *config.Config) (*PoolIdentifier, error) {
+
+
+	var pg *postgresql.Postgresql
+	var err error
+	if config.Postgres != "" {
+		pg, err = postgresql.New(config.Postgres)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not create postgresql")
+		}
+		// err = pg.CreateValidatorPoolTable()
+		// if err != nil {
+		// 	return nil, errors.Wrap(err, "error creating pool table to store data")
+		// }
+	}
+
+	return &PoolIdentifier{postgresql:  pg,config:      config,}, nil
+	
+	
+}
+func (a *PoolIdentifier) Run() {
+	_ = ReadDepositorAddresses(a)
+}
+func ReadDepositorAddresses(a *PoolIdentifier) (error) {
+	var dir string = "./poolDepositors/"
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return errors.Wrap(err, "could not read files in directory "+ dir)
+	}	
+	for _, file := range files {
+		var fileName string = file.Name()
+		var filePath string = dir+fileName
+		var poolName string = fileName[0:len(fileName)-4] // remove .txt extension
+		f, err := os.Open(filePath)
+		log.Info("Getting validators for pool: ", poolName)
+		if err != nil {
+			return errors.Wrap(err, "could not read file "+ fileName)
+		}
+		fmt.Println(fileName)
+	
+		defer f.Close()
+	
+		scanner := bufio.NewScanner(f)
+		var depositors []string
+		for scanner.Scan() {
+			// do something with a line
+			fmt.Printf("line: %s\n", scanner.Text())
+			depositors = append(depositors, scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			return errors.Wrap(err, "could not get validators from depositor addresses corresponding to file "+ fileName)
+		}
+
+		validators, err := a.postgresql.GetPoolValidators(poolName, depositors)
+		if err != nil {
+			return errors.Wrap(err, "could not get pool validators for pool"+poolName+" from postgresql")
+		}		
+		err = WritePoolValidatorsFile(poolName, validators)
+		if err != nil {
+			return errors.Wrap(err, "could not write validators from pool "+poolName +" to .txt file ")
+		}
+    }
+
+	return nil
+}
+func WritePoolValidatorsFile(pool string, validators []string) (error) {
+	var dir string = "./poolValidators/"
+	var fileName string = dir+pool+".txt"
+	f, err := os.Create(fileName)
+	if err != nil {
+		return errors.Wrap(err, "could not create file "+ fileName)
+	}
+	defer f.Close()
+
+	for _, validator := range validators {
+		_, err := f.WriteString(validator + "\n")
+		if err != nil {
+			return errors.Wrap(err, "could not write validator "+ validator + " to file "+ fileName)
+		}
+	}
+	return nil
+}
